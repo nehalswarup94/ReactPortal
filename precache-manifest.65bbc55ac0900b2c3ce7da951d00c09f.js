40602a49a8f94faf5185c0755b1799310a2202ae
nehalswarup94 <54302683+nehalswarup94@users.noreply.github.com>
2020-07-16 18:25:56 +0530

    Updates

---
+++ precache-manifest.65bbc55ac0900b2c3ce7da951d00c09f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35b5e6fc88d272cfa5a3244432147ab0",
+    "url": "/myportal/index.html"
+  },
+  {
+    "revision": "aad684a62f022d14d022",
+    "url": "/myportal/static/css/main.08599511.chunk.css"
+  },
+  {
+    "revision": "7ee16ddd1dc34f5fea9f",
+    "url": "/myportal/static/js/2.5c616f49.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/myportal/static/js/2.5c616f49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aad684a62f022d14d022",
+    "url": "/myportal/static/js/main.41638e7e.chunk.js"
+  },
+  {
+    "revision": "56d195dd519754f066cc",
+    "url": "/myportal/static/js/runtime-main.9ad5144d.js"
+  }
+]);
